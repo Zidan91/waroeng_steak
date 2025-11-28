20d44537402b6a09e03258bec475f10bb11335b7
Zidan <muchtizidan@gmail.com>
2025-11-28 08:18:44 +0700

    fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
